ac3ac05fa743d0ef58b771f50e06555ca649dfbe
Phonekham <phone.madridista@gmail.com>
2022-07-23 17:15:09 +0700

    map marker

---
+++ src/features/map/components/map-callout.component.jsx
@@ -0,0 +1,5 @@
+import { CompactRestaurantInfo } from "../../../components/restaurant/compact.restaurant-info.component";
+
+export const MyCallout = ({ restaurant }) => (
+  <CompactRestaurantInfo restaurant={restaurant} />
+);
